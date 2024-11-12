060d25e08f2f388410a40427b1a2095d62b90641
RXAtomix <roro0510@laposte.net>
2024-11-12 20:09:20 +0100

    Update server.js

--- server.js
+++ server.js
@@ -29,6 +29,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/users", authMiddleware, userRouter);
+app.use('/api/articles', articlesRouter);
 app.post("/login", usersController.login);
 
 app.use("/", express.static("public"));
