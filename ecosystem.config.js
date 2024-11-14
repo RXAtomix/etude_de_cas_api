41412bb69ed090b2b9ff2a60a4a0366a7e37bbb2
RXAtomix <roro0510@laposte.net>
2024-11-14 14:14:25 +0100

    modification du ficher ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,18 @@
 module.exports = {
-  apps: [
-    {
-      name: "app",
-      script: "./www/app.js",
-      env_production: {
-        NODE_ENV: "production",
-      },
-    },
-  ],
+    apps: [
+        {
+            name: "app",
+            script: "./www/app.js",
+            instances: 3,
+            exec_mode: "cluster",
+            max_memory_restart: "200M",
+            error_file: "./logs/err.log",
+            out_file: "./logs/out.log",
+            log_file: "./logs/app.log",
+            combine_logs: true,
+            env_production: {
+                NODE_ENV: "production",
+            },
+        },
+    ],
 };
