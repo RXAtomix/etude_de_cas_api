28733258523150b202a4b7aaca4bb68b222e7565
RXAtomix <roro0510@laposte.net>
2024-11-14 10:40:01 +0100

    fix du auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,18 +1,32 @@
 const UnauthorizedError = require("../errors/unauthorized");
 const jwt = require("jsonwebtoken");
 const config = require("../config");
+const User = require("../api/users/users.model");
 
 module.exports = (req, res, next) => {
-  try {
-    const token = req.headers["x-access-token"];
-    if (!token) {
-      throw "not token";
+    try {
+        const token = req.headers["x-access-token"];
+        if (!token) {
+            throw "Token non fourni";
+        }
+
+        const decoded = jwt.verify(token, config.secretJwtToken);
+
+        // Rechercher l'utilisateur dans la base de données avec une promesse
+        User.findById(decoded.userId)
+            .then((user) => {
+                if (!user) {
+                    throw "Utilisateur non trouvé";
+                }
+
+                // Passer toutes les informations de l'utilisateur dans req
+                req.user = user;
+                next();
+            })
+            .catch((err) => {
+                next(new UnauthorizedError(err));
+            });
+    } catch (message) {
+        next(new UnauthorizedError(message));
     }
-    const decoded = jwt.verify(token, config.secretJwtToken);
-      req.user = decoded;
-      req.user = user; // Passer toutes les informations utilisateur dans req
-    next();
-  } catch (message) {
-    next(new UnauthorizedError(message));
-  }
 };
